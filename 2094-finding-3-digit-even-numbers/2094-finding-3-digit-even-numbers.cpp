291c83b508309b47ee0f973b854b2913998b620c
Ayush Kumar <ayushstack04@gmail.com>
2026-07-23 17:16:05 +0530

    Time: 387 ms (22.02%), Space: 14.1 MB (34.76%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> findEvenNumbers(vector<int>& digits) {
         int n=digits.size();
         set<int>set;
-        // vector <int> st;
         for (int i=0;i<n;i++){
             for (int j =0;j<n;j++){
                 for (int k =0;k<n;k++){
@@ -15,9 +14,6 @@ public:
                 }
             }
         }
-        // for (int x:set){
-        //     st.push_back(x);
-        // }
         return vector<int>(set.begin(),set.end());
     }
 };
